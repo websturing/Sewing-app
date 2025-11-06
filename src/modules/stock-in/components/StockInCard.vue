c30eb8893b78b6809ab952ede6865a0993a366ae
websturing <websturing.project@gmail.com>
2025-11-06 14:47:26 +0700

    ✨ feat(stock-in): enable navigation to gl detail from stock in card
    - add click event handler to gl number in stock in card component
    - integrate useGLPage composable to navigate to gl detail page when gl number is clicked

--- src/modules/stock-in/components/StockInCard.vue
+++ src/modules/stock-in/components/StockInCard.vue
@@ -3,7 +3,8 @@
         <n-card :class="['shadow-xs cursor-pointer hover:!bg-gray-50']" :content-style="{ padding: 0 }">
             <div class="flex justify-between px-4 py-2">
                 <div>
-                    <p class="font-semibold text-blue-500">{{ props.item.glNo }}</p>
+                    <p class="font-semibold text-blue-500" @click="navigateToGLDetail(props.item.glNo)">{{
+                        props.item.glNo }}</p>
                     <span class="text-xs text-gray-400">
                         {{ props.item.lineNames }}
                     </span>
@@ -24,12 +25,13 @@
     </div>
 </template>
 <script setup lang="ts">
+import { useGLPage } from '@/modules/gls/composables/gls.page';
 import type { StockInSummaryGroupByGlNumber } from '@/modules/stock-in/schemas/stockInSummary.api.schema';
-
 
 interface Props {
     item: StockInSummaryGroupByGlNumber
 }
 const props = withDefaults(defineProps<Props>(), {})
 
+const { navigateToGLDetail } = useGLPage()
 </script>
