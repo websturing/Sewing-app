a744f64d7835f93d3e575b95e4cc666b39d62d15
websturing <websturing.project@gmail.com>
2025-12-02 15:40:13 +0700

    ♻️ refactor(types): make from and to properties nullable in MetaSchema
    - update `MetaSchema` to allow `from` and `to` properties to be nullable
    - ensures correct validation for pagination responses where these properties might be null, such as when the data set is empty

--- src/types/metaPagination.ts
+++ src/types/metaPagination.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 export const LinksSchema = z.object({}).passthrough()
 export const MetaSchema = z.object({
-    currentPage: z.number(),
-    from : z.number(),
-    lastPage: z.number(),
-    path : z.string(),
-    perPage: z.number(),
-    to : z.number(),
-    total: z.number()
+  currentPage: z.number(),
+  from: z.number().nullable(),
+  lastPage: z.number(),
+  path: z.string(),
+  perPage: z.number(),
+  to: z.number().nullable(),
+  total: z.number()
 }).passthrough()
 
-  export type Links = z.infer<typeof LinksSchema>
-  export type Meta = z.infer<typeof MetaSchema>
+export type Links = z.infer<typeof LinksSchema>
+export type Meta = z.infer<typeof MetaSchema>
